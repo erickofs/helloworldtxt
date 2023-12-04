f2f28bd94f8dccad0c535bf47f6adfede717c043
Erick Fernandes <80919235+erickofs@users.noreply.github.com>
2023-12-04 20:55:14 -0300

    Update helloworldtxt.py
    first line comment updated

--- erickofs/helloworldtxt.py
+++ erickofs/helloworldtxt.py
@@ -1,4 +1,4 @@
-# Open hello.txt file with explicit encoding
+# Open hello.txt file with explicit encoding and strip lines
 with open("hello.txt", "r+", encoding="utf-8") as file: lines = [line.strip() for line in file.readlines()]
 # print only lines that are not empty or commented
 print("\n".join([line for line in lines if line != "" and not line.startswith("#")]))
